9da628970f696ec554702d21fe1833dbd4411ed8
Yerlin Matu <yerlinmatu@gmail.com>
2023-03-15 09:15:08 -0500

    Refactoring and improvements to homepage code
    added "strict mode", used "getElementById" instead of "querySelector", fixed a typo, used "textContent" instead of "+=", added comments for clarity, used object destructuring, added a startup function, and improved code readability."

--- src/script.js
+++ src/script.js
@@ -1,33 +1,36 @@
-!function () {
+(function() {
+  'use strict';
 
-  const api = {
-    endpoint: 'https://api.ipify.org/?format=json',
-    platform: navigator.platform,
-    internet: navigator.onLine,
-  } 
+  const endpoint = 'https://api.ipify.org/?format=json';
+  const platformInfo = navigator.platform;
+  const isOnline = navigator.onLine;
   
-  const ipPublicText = document.querySelector('#ip'),
-        ipPrivateText = document.querySelector('#ip-local'),
-        plataformText = document.querySelector('#plataform'),
-        internetText = document.querySelector('#internet');
-        
-  axios.get(api.endpoint).then( response => {
-    const ip = response.data.ip
-    ipPublicText.innerHTML += ip;
-  }).catch( err => IpText.innerHTML = err.toString());
-  
-  plataformText.innerHTML += api.platform;
-  internetText.innerHTML += api.internet ? 'Conectado' : 'Desconectado';  
+  const publicIpText = document.getElementById('public-ip');
+  const localIpText = document.getElementById('local-ip');
+  const platformText = document.getElementById('platform-info');
+  const connectionText = document.getElementById('connection-status');
+
+  axios.get(endpoint)
+    .then(response => {
+      const publicIp = response.data.ip;
+      publicIpText.textContent = publicIp;
+    })
+    .catch(error => {
+      publicIpText.textContent = error.toString();
+    });
+
+  platformText.textContent = platformInfo;
+  connectionText.textContent = isOnline ? 'Connected' : 'Disconnected';
 
   window.RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    const pc = new RTCPeerConnection({iceServers:[]}), noop = () => {};      
-    pc.createDataChannel('');    //create a bogus data channel
-    pc.createOffer(pc.setLocalDescription.bind(pc), noop);    // create offer and set local description
-    pc.onicecandidate = ice => {  //listen for candidate events
-        if(!ice || !ice.candidate || !ice.candidate.candidate)  return;
-        const ipLocal = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
-        ipPrivateText.innerHTML += ipLocal;
-        pc.onicecandidate = noop;
-    };
-
-}();
+  const pc = new RTCPeerConnection({iceServers: []});
+  const noop = () => {};      
+  pc.createDataChannel('');    
+  pc.createOffer(pc.setLocalDescription.bind(pc), noop);    
+  pc.onicecandidate = ice => {
+    if (!ice || !ice.candidate || !ice.candidate.candidate)  return;
+    const localIp = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/.exec(ice.candidate.candidate)[1];
+    localIpText.textContent = localIp;
+    pc.onicecandidate = noop;
+  };
+})();
